4cb10adea13a6401637e998f1f9310cc24338384
Yahir Cardona <yahir.cardona@gmail.com>
2020-08-03 15:48:01 -0600

    create method added to controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,20 @@
+# frozen_string_literal: true
+
 class UsersController < ApplicationController
-    def new 
+  def new
+    @user = User.new
+ end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new
     end
-    def create
-    end
+  end
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 end
