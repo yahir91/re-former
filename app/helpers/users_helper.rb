4603c60537777036e89e53fe611b4f69d1145d46
Yahir Cardona <yahir.cardona@gmail.com>
2020-08-04 15:47:40 -0600

    partial created and logic removed

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,11 @@
 module UsersHelper
+    def errors(user)    
+        error = []
+      if @user.errors.any?       
+          @user.errors.full_messages.each do |msg|
+          error << "<li>#{msg}</li>"
+          end
+      end
+      error.join
+    end
 end
