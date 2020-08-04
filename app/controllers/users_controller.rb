43b9c6f03968da0f3cfad33e8d494379d417b1e6
Yahir Cardona <yahir.cardona@gmail.com>
2020-08-04 09:31:49 -0600

    form created with form_for

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,11 +3,11 @@
 class UsersController < ApplicationController
   def new
     @user = User.new
- end
+  end
 
   def create
-    # @user = User.new(user_params)
-    @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    @user = User.new(user_params)
+    # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
 
     if @user.save
       redirect_to new_user_path
@@ -15,6 +15,8 @@ class UsersController < ApplicationController
       render :new
     end
   end
+
+  private
 
   def user_params
     params.require(:user).permit(:username, :email, :password)
